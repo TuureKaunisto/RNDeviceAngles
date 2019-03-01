f84ec6a9746a47c16b4c3c45f6148e1991aa9b3e
Tuure Kaunisto <tuure.kaunisto@grano.fi>
2019-03-01 16:39:21 +0200

    Try out suggested solution to the "Redefinition of RCTMethodInfo" problem

--- RNDeviceAngles/DeviceAngles.h
+++ RNDeviceAngles/DeviceAngles.h
@@ -5,7 +5,11 @@
 //  Created by Cristian Szwarc on 20/08/16.
 
 #import <Foundation/Foundation.h>
-#import "RCTBridgeModule.h"
+#if __has_include(<React/RCTBridgeModule.h>)
+  #import <React/RCTBridgeModule.h>
+#else
+  #import "RCTBridgeModule.h"
+#endif
 #import <CoreMotion/CoreMotion.h>
 
 @interface DeviceAngles : NSObject <RCTBridgeModule> {
